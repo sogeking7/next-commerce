8e3aa671ecffc6920bbd5031103366f2babf2b36
Kaiyrkhan Kairolla <111349192+sogeking7@users.noreply.github.com>
2023-09-29 04:09:52 +0600

    fix(middleware): open api

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,9 @@ import { authMiddleware } from '@clerk/nextjs'
 
 export default authMiddleware({
   // "/" will be accessible to all users
-  publicRoutes: ['/', '/api/webhooks/user', '/api/product'],
+  publicRoutes: ['/', '/api/webhooks/user', '/api/product', '/api/user/cart'],
 })
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)', '/api/webhooks/user'],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/',],
 }
